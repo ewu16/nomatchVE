70d141c7e608ce7dfd2dfd5abc2233ebbdb42c04
Emily Wu <emily.wu2@emory.edu>
2025-03-05 17:38:06 -0500

    update docs

--- R/compute_ve.R
+++ R/compute_ve.R
@@ -55,8 +55,8 @@ compute_ve_t0 <- function(fit_0, fit_1, time_name, t0, tau, gp_list){
 #'
 #' @return {A named numeric vector containing point estimates for
 #'  `risk_0, risk_1`, and `ve`}
-#' @export
 #'
+#' @noRd
 add_ve <- function(psi_bar){
 
     ve <- unname(1 - psi_bar["risk_1"]/psi_bar["risk_0"])
