9ad66a4d4c18942f296eee2e36c8a016dc0e97a1
Emily Wu <emily.wu2@emory.edu>
2025-10-18 20:00:22 -0400

    Update compute_psi_bar function names and internal function calls

--- R/compute_psi_bar.R
+++ R/compute_psi_bar.R
@@ -1,64 +1,43 @@
-#' Compute proposed causal VE estimand given the fitted survival models at various
-#' time points of interest
+#' Compute overall cumulative incidences at multiple timepoints
 #'
 #' @description
-#' `compute_ve()` returns the VE estimates at multiple time points. This is function is a
-#' wrapper around `compute_ve_t0()`.
+#' Wrapper that computes cumulative incidences at multiple
+#' timepoints. Calls [compute_psi_bar_t0()]  internally for each timepoint.
+#' Models are fitted once before calling this function to allow efficient evaluation
+#' at multiple timepoints without refitting.
 #'
-#' @inheritParams compute_psi_d
-#' @inheritParams nomatchVE
+#' @inheritParams compute_psi_dx_t0
 #'
-#' @return A matrix of estimates where the columns are the terms `risk_0, risk_1`, and `ve`
+#' @return A matrix of estimates where the columns are the terms `cuminc_0` and `cuminc_1`,
 #' and the rows are the time points of interest.
+#' @details
+#'  This function expects models already fitted via [fit_model_0()] and
+#' [fit_model_1()]. It performs G-computation by predicting and marginalizing over
+#' conditional risks.
+#'
 #' @export
 #'
-compute_ve <- function(fit_0, fit_1, time_name, times, tau, gp_list){
-    ve <- sapply(times, \(t){
-        compute_ve_t0(fit_0, fit_1, time_name, t0 = t, tau, gp_list)
+compute_psi_bar_times <- function(fit_0, fit_1, time_name, times, tau, newdata, gp_list){
+    out <- sapply(times, \(t){
+        compute_psi_bar_t0(fit_0, fit_1, time_name, t0 = t, tau, newdata, gp_list)
     })
-    colnames(ve) <- times
-    t(ve)
+    colnames(out) <- times
+    t(out)
 }
 
-
-
-#' Compute proposed causal VE estimand given the fitted survival models at a single
-#' time points of interest
+#' Compute overall cumulative incidences at a single timepoint
 #'
-#' @inheritParams compute_psi_d
-#' @inheritParams nomatchVE
-#'
-#' @return A named numeric vector containing point estimates for
-#'  `risk_0, risk_1`, and `ve`
-#'
-compute_ve_t0 <- function(fit_0, fit_1, time_name, t0, tau, gp_list){
-
-    psi_d <- compute_psi_d(fit_0, fit_1, time_name, t0, tau, gp_list)
-    psi_bar <- compute_psi_bar(psi_d, gp_list)
-
-    # if(any(is.na(psi_bar))){
-    #     cat("psi_bar: ", psi_bar, "\n")
-    #     print(summary(fit_0))
-    #     print(summary(fit_1))
-    # }
-
-    add_ve(psi_bar)
-}
-
-#' Calculate VE based on risk_0 and risk_1
-#'
-#' This function computes VE as the proportion reduction in risk due to
-#' vaccination (ie. VE = 1 - risk_0 / risk_1)
-#'
-#' @param psi_bar {A named numeric vector containing point estimates for
-#'  `risk_0, risk_1`}
-#'
-#' @return {A named numeric vector containing point estimates for
-#'  `risk_0, risk_1`, and `ve`}
-#'
+#' @inheritParams compute_psi_dx_t0
+#' @inheritParams  marginalize_psi_dx_t0
+#' @return Named numeric vector with `cuminc_0`, `cuminc_1`
+#' @keywords internal
 #' @noRd
-add_ve <- function(psi_bar){
+compute_psi_bar_t0 <- function(fit_0, fit_1, time_name, t0, tau, newdata, gp_list){
+    # Step 1: Get conditional risks for all (d, x)
+    psi_dx <- compute_psi_dx_t0(fit_0, fit_1, time_name, t0, tau, newdata)
 
-    ve <- unname(1 - psi_bar["risk_1"]/psi_bar["risk_0"])
-    c(psi_bar, "ve" = ve)
+    # Step 2: Marginalize to population level
+    psi_bar <- marginalize_psi_dx_t0(psi_dx, gp_list)
+
+    psi_bar
 }
