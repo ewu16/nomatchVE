a0864b0615048d421910ea065439acb125f6bf4a
Emily Wu <emily.wu2@emory.edu>
2025-10-21 14:16:05 -0400

    Remove cox estimation for matching and only support KM estimation

--- R/get_one_matching_ve.R
+++ R/get_one_matching_ve.R
@@ -18,9 +18,6 @@ get_one_matching_ve <- function(matched_data,
                                 tau,
                                 eval_times,
                                 method = "km",
-                                censor_time = NULL,
-                                adjust = NULL,
-                                separate = TRUE,
                                 keep_models = TRUE){
 
 
@@ -31,17 +28,48 @@ get_one_matching_ve <- function(matched_data,
                                         exposure_time = exposure_time,
                                         tau = tau)
 
-    estimates <- compute_marginal_ve(adata = matched_adata,
-                                     adata_outcome_name = "match_T",
-                                     adata_event_name =  paste0("match_", outcome_status),
-                                     adata_trt_name = paste0("match_", exposure),
-                                     method = method,
-                                     adjust = adjust,
-                                     eval_times = eval_times,
-                                     censor_time = censor_time,
-                                     separate = separate,
-                                     keep_models = keep_models)
-    estimates
+   out <- compute_km_ve(adata = matched_adata,
+                        adata_outcome_name = "match_T",
+                        adata_event_name =  paste0("match_", outcome_status),
+                        adata_trt_name = paste0("match_", exposure),
+                        eval_times = eval_times,
+                        keep_models = keep_models)
+   return(out)
+
+
+}
+#' Get the marginal cumulative incidence in the treated and untreated
+#' groups based on Kaplan Meier estimation.
+#'
+#' @inheritParams compute_marginal_ve
+#' @inherit compute_marginal_ve return
+#'
+#' @keywords internal
+
+compute_km_ve <- function(adata,
+                          adata_outcome_name,
+                          adata_event_name,
+                          adata_trt_name,
+                          eval_times,
+                          keep_models = TRUE){
+
+    outcome <- paste0("survival::Surv(", adata_outcome_name, ",", adata_event_name, ")")
+    km_fit <- survival::survfit(stats::reformulate(adata_trt_name, response = outcome) , data = adata)
+    surv_probs <- summary(km_fit, times = eval_times)
+    cuminc_0 <- 1 - surv_probs$surv[surv_probs$strata == levels(surv_probs$strata)[1]]
+    cuminc_1 <- 1 - surv_probs$surv[surv_probs$strata == levels(surv_probs$strata)[2]]
+    rr <- cuminc_1/cuminc_0
+    ve <- 1 - rr
+
+    est  <- cbind(cuminc_0, cuminc_1, "risk_ratio" = rr, "vaccine_effectiveness" = ve)
+
+    rownames(est) <- eval_times
+    out <- list(pt_estimates = est)
+
+    if(keep_models){
+        out$models <- km_fit
+    }
+    return(out)
 }
 
 
